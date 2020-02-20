9b69b1da25e08ccc9dd9c9feb0d47c664bb34989
bbaa_bbaa <bbaa_bbaa@qq.com>
2020-02-20 18:07:33 +0800

    处理特殊问题

--- identifior2.js
+++ identifior2.js
@@ -1,5 +1,7 @@
 class Identifior {
-  constructor() {}
+  constructor() {
+    this.SpecialIndex = 0;
+  }
   async load() {
     await fetch("./data.json")
       .then(value => value.json())
@@ -17,7 +19,14 @@ class Identifior {
     }
   }
   initQuestions() {
-    this.Questions = Object.values(this.RawData.questions).map(value => new Question(value, this));
+    this.SimpleQuestions = Object.values(this.RawData.questions)
+      .filter(value => !value.special)
+      .map(value => new Question(value, this));
+    this.SpecialQuestion = {};
+    let SpecialQuestion = Object.values(this.RawData.questions).filter(value => value.special);
+    for (let value of SpecialQuestion) {
+      SpecialQuestion[value.characters[0]] = value;
+    }
     this.askdQuestions = [];
     let Characters = new Set();
     for (let question of Object.values(this.RawData.questions)) {
@@ -31,27 +40,10 @@ class Identifior {
   getQuestion() {
     //通过Score获取目前最符合的问题
     this.CharacterList.sort((a, b) => b.Score - a.Score); // 将对象转为数组并从大到小进行排序;
-    if (this.Questions.length == 0) return false; // 回答结束判断
-    let LastValue;
-    if (
-      this.CharacterList.slice(0, 3).every((value, index) => {
-        //如果前三分数不同 则结束问题
-        if (index == 0) {
-          LastValue = value.Score;
-          return true;
-        } else {
-          if (LastValue != value.Score) {
-            LastValue = value.Score;
-            return true;
-          }
-          return false;
-        }
-      })
-    )
-      return false; // 回答结束判断
+    if (this.SimpleQuestions.length == 0) return this.GetSpecialQuestion(); // 普通问题回答结束判断
     const MiddleScore = this.CharacterList[Math.round(this.CharacterList.length * 0.5)].Score;
     let ScoreHighCharacters = this.CharacterList.filter(value => value.Score >= MiddleScore); //过滤出 Score >= 中位数的人物
-    const ChoiceQuestion = this.Questions.map(value => {
+    const ChoiceQuestion = this.SimpleQuestions.map(value => {
       const Score = ScoreHighCharacters.reduce((previousValue, currentValue) => {
         if (value.Characters.includes(currentValue.Name)) {
           // 如果问题包含这个人物的话
@@ -61,8 +53,17 @@ class Identifior {
       }, 0); //累加器
       return [Score, value];
     }).sort((a, b) => b[0] - a[0] /* [0]是分数 */)[0 /*取分数最高*/][1 /*问题*/]; // 给每个问题进行打分并且排序，取包含相关度高人物最多的问题 结构 [Score,Question]
-    this.askdQuestions.push(this.Questions.splice(this.Questions.indexOf(ChoiceQuestion), 1));
+    this.askdQuestions.push(this.SimpleQuestions.splice(this.SimpleQuestions.indexOf(ChoiceQuestion), 1));
     return ChoiceQuestion;
+  }
+  GetSpecialQuestion() {
+    if (this.SpecialIndex < 5) {
+      this.askdQuestions.push(this.SpecialQuestion[this.CharacterList[0].Name]);
+      const Question = this.SpecialQuestion[this.CharacterList[0].Name];
+      delete this.SpecialQuestion[this.CharacterList[0].Name];
+      return Question;
+    }
+    return false;
   }
 }
 class Question {
@@ -73,13 +74,23 @@ class Question {
     this.special = question.special;
   }
   yes() {
-    for (let characterName of this.Characters) {
-      this.Identifior.Characters[characterName].Score += 1 / this.Characters.length;
+    if (this.special) {
+      this.Identifior.Characters[characterName].Score += Infinity;
+      this.Identifior.SpecialIndex = 5;
+    } else {
+      for (let characterName of this.Characters) {
+        this.Identifior.Characters[characterName].Score += 1 / this.Characters.length;
+      }
     }
   }
   no() {
-    for (let characterName of this.Characters) {
-      this.Identifior.Characters[characterName].Score *= 0.7;
+    if (this.special) {
+      this.Identifior.Characters[characterName].Score -= Infinity;
+      this.Identifior.SpecialIndex = 5;
+    } else {
+      for (let characterName of this.Characters) {
+        this.Identifior.Characters[characterName].Score *= 0.7;
+      }
     }
   }
 }
