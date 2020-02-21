657636475bc84d88363cfeffaa0a25c39e240f4f
bbaa_bbaa <bbaa_bbaa@qq.com>
2020-02-21 22:34:16 +0800

    补充注释

--- identifior2.js
+++ identifior2.js
@@ -21,8 +21,21 @@ class Identifior {
   }
   initQuestions() {
     this.SimpleQuestions = Object.values(this.RawData.questions)
-      .filter(value => !value.special)
-      .map(value => new Question(value, this));
+      .filter(
+        value => !value.special
+      ) /*
+       filter(过滤器，滤波器?)
+       将每个值传入一个函数进行判断，返回true即保留
+       这里用于过滤非特殊问题
+       https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+       */
+      .map(
+        value => new Question(value, this)
+      ); /*
+        map 将每个数组元素用一个函数进行处理并且替换原值为返回值
+        这里用于转换每个问题为Question对象
+        https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+      */
     this.SpecialQuestion = {};
     let SpecialQuestion = Object.values(this.RawData.questions).filter(value => value.special);
     for (let value of SpecialQuestion) {
@@ -40,7 +53,7 @@ class Identifior {
   }
   calcWeight() {
     //计算每个角色的(权?)
-    let AllQuestions = this.SimpleQuestions.concat(Object.values(this.SpecialQuestion));
+    let AllQuestions = this.SimpleQuestions.concat(Object.values(this.SpecialQuestion)); //拼接数组
     let CharData = {}; // 每个角色在问题中的占比
     for (let question of AllQuestions) {
       if (question.special) {
@@ -72,14 +85,17 @@ class Identifior {
     if (this.SimpleQuestions.length == 0) {
       let NowQuestion = this.GetSpecialQuestion();
       if (!NowQuestion) this.CharacterList.sort((a, b) => b.Score - a.Score); // 从大到小进行排序;
-      if (Math.max(...this.CharacterList.map(a => a.Score)) > 99) return false;
+      if (Math.max(...this.CharacterList.map(a => a.Score)) > 99) return false; //当最大可信度>99%结束提问
       return NowQuestion;
     } // 普通问题回答结束判断
-    this.CharacterList.sort((a, b) => b.Score - a.Score); // 从大到小进行排序;
+    this.CharacterList.sort((a, b) => b.Score - a.Score); // 从大到小进行排序（人物分数）;
     const MiddleScore = this.CharacterList[Math.round(this.CharacterList.length * 0.5)].Score;
     let ScoreHighCharacters = this.CharacterList.filter(value => value.Score >= MiddleScore); //过滤出 Score >= 中位数的人物
     const ChoiceQuestion = this.SimpleQuestions.map(value => {
-      const Score = ScoreHighCharacters.reduce((previousValue, currentValue) => {
+      const Score = ScoreHighCharacters.reduce((
+        previousValue /*　之前累加器返回的结果　*/,
+        currentValue /* 现在处理的数组元素 */
+      ) => {
         if (value.Characters.includes(currentValue.Name)) {
           // 如果问题包含这个人物的话
           return ++previousValue; //返回之前的值+1
@@ -121,24 +137,25 @@ class Question {
   yes() {
     for (let characterName of this.Characters) {
       this.Identifior.Characters[characterName].Score +=
-        (1 / this.Characters.length) * this.Identifior.Characters[characterName].Weight;
+        (1 / this.Characters.length) * this.Identifior.Characters[characterName].Weight; //加分
     }
   }
   no() {
     if (this.special) {
       for (let characterName of this.Characters) {
-        this.Identifior.Characters[characterName].Score *= 0.8;
+        this.Identifior.Characters[characterName].Score *= 0.8; //排除
         this.Identifior.SpecialIndex = 0;
       }
     } else {
       let addScoreCharacters = this.Identifior.CharacterList.filter(value => !this.Characters.includes(value.Name));
       for (let item of addScoreCharacters) {
-        item.Score += (1 / addScoreCharacters.length) * item.Weight;
+        item.Score += (1 / addScoreCharacters.length) * item.Weight; //加分
       }
     }
   }
 }
 class Character {
+  // 这个对象没有任何原型功能
   constructor(name) {
     this.Name = name;
     this.Score = 0;
